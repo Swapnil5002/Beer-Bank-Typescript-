580709d772e5d41f17bf757ca979af9540b84faf
Swapnil5002 <52300853+Swapnil5002@users.noreply.github.com>
2023-09-20 23:49:36 +0530

    Folder setup

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,6 @@ import { FC } from 'react';
 
 import './style.css';
 
-export const App: FC<{ name: string }> = ({ name }) => {
-  return (
-    <div>
-      <h1>Hello {name}!</h1>
-      <p>Start editing to see some magic happen :)</p>
-    </div>
-  );
+export const App = () => {
+  return <div>This is initial setup!</div>;
 };
